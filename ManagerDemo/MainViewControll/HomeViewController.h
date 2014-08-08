651a3b8c21d9556da1b1ae97c052bbe6900dfb1b
wu736139669 <736139669@qq.com>
2014-08-08 12:06:12 +0800

    add homeviewcontroll

---
+++ ManagerDemo/MainViewControll/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  ManagerDemo
+//
+//  Created by xmfish on 14-8-8.
+//  Copyright (c) 2014年 ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ ManagerDemo/MainViewControll/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  ManagerDemo
+//
+//  Created by xmfish on 14-8-8.
+//  Copyright (c) 2014年 ash. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
